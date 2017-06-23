73aeb215ef2aa3db1c451ae3a27846caeec6d6c6
Alain Cardoso <alain_kardoso@hotmail.com>
2017-06-22 23:08:10 -0300

    Criando a solução para o projeto e adicionando a base de dados (ainda sem tabelas);

---
+++ RestauranteProjetoWeb/RestauranteProjetoWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestauranteProjetoWeb.Startup))]
+namespace RestauranteProjetoWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
