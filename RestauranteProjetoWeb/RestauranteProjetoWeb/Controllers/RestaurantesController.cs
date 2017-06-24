48ce5a82f441683ee2bf51c7087a831e48ef70fe
Renan <re.nan.mb@hotmail.com>
2017-06-24 14:43:03 -0300

    Instânciando RestauranteController (Modo wizard), Ajustando Views(css/boodstrap) Create e Delete Função Delete(RestauranteController) alterada para exclusão lógica Elaborei tbm um css meu .

---
+++ RestauranteProjetoWeb/RestauranteProjetoWeb/Controllers/RestaurantesController.cs
@@ -0,0 +1,127 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using BaseModels;
+using RestauranteProjetoWeb.Models;
+
+namespace RestauranteProjetoWeb.Controllers
+{
+    public class RestaurantesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Restaurantes
+        public ActionResult Index()
+        {
+            return View(db.Restaurantes.ToList());
+        }
+
+        // GET: Restaurantes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurante restaurante = db.Restaurantes.Find(id);
+            if (restaurante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurante);
+        }
+
+        // GET: Restaurantes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Restaurantes/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RestauranteID,NameOnwer,Emailaddress,Phonenumber,RestaurantName,Descricao,RestaurantCountry,RestaurantCity,RestaurantState,RestaurantPostalCode,Ativo")] Restaurante restaurante)
+        {
+            if (ModelState.IsValid)
+            {
+                restaurante.Ativo = true;
+                db.Restaurantes.Add(restaurante);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurante);
+        }
+
+        // GET: Restaurantes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurante restaurante = db.Restaurantes.Find(id);
+            if (restaurante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurante);
+        }
+
+        // POST: Restaurantes/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RestauranteID,NameOnwer,Emailaddress,Phonenumber,RestaurantName,Descricao,RestaurantCountry,RestaurantCity,RestaurantState,RestaurantPostalCode,Ativo")] Restaurante restaurante)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(restaurante).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurante);
+        }
+
+        // GET: Restaurantes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurante restaurante = db.Restaurantes.Find(id);
+            if (restaurante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurante);
+        }
+
+        // POST: Restaurantes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Restaurante restaurante = db.Restaurantes.Find(id);
+            // db.Restaurantes.Remove(restaurante);
+            restaurante.Ativo = false;
+            db.Entry(restaurante).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
